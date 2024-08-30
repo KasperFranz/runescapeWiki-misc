0b6163854bf1d30cb0e862b180e2e962e90bf272
Kasper Sanguesa-Franz <kasper@franz.guru>
2024-08-30 09:57:20 +0100

    Location updates

--- getLocationsForIds.js
+++ getLocationsForIds.js
@@ -2,29 +2,44 @@ const fs = require('node:fs');
 
 
 // Example usage
-const ids = [120404];
-const sceneryName = 'Chest of drawers'
+const ids = [	125233];
+const sceneryName = 'Operating table'
 
 
 const locations = [
     {name: '[[Senntisten]]', minX: 1537, maxX: 2100, minY: 1150, maxY: 1360, mapId: '-1'},
     {name: '[[The Zamorakian Undercity]]', minX: 1600, maxX: 1790, minY: 1500, maxY: 1790, mapId: 740},
     {name: '[[Senntisten Asylum]]', minX: 1930, maxX: 2038, minY: 1420, maxY: 1530, mapId: 738},
-    {name: '[[Daemonheim]]', minX: 3374, maxX: 3525, minY: 3607, maxY: 3796, mapId: '-1'}
+    {name: '[[Daemonheim]]', minX: 3374, maxX: 3525, minY: 3607, maxY: 3796, mapId: '-1'},
+    {name: '[[Freneskae]]', minX: 2560, maxX: 2816, minY: 12287, maxY: 12607, mapId: '-1'},
+    {name: '[[Freneskae#The_Ritual_Site|Freneskae Ritual Site]]', minX: 2428, maxX: 2561, minY: 12350, maxY: 12547, mapId: '-1'},
+    {name: '[[Mort Myre Swamp]]', minX: 3398, maxX: 3646, minY: 3252, maxY: 3528, mapId: '-1'},
+    {name: '[[The beach]]', minX: 3140, maxX: 3193, minY: 3203, maxY: 3260, mapId: '-1'},
+    {name: '[[Al Kharid]]', minX: 3266, maxX: 3334, minY: 3133, maxY: 3221, mapId: '-1'},
+
 ];
 
+const ignoredLocations =
+    [
+        {name: 'Senntisten instance 1', minX: 1595, maxX: 2049, minY: 1791, maxY: 2054, mapId: '-1'},
+        {name: 'Senntisten instance 2', minX: 514, maxX: 900, minY: 129, maxY: 529, mapId: '-1'},
+        {name: 'Mort Myre Swamp instance', minX: 1984, maxX: 2050, minY: 4990, maxY: 5056, mapId: '-1'},
+        {name: 'Mort Myre Swamp instance', minX: 3778, maxX: 3969, minY: 7550, maxY: 7746, mapId: '-1'},
+    ];
 const template = `{{ObjectLocLine
-|name={{NAME}}
+|name = {{NAME}}
 |loc = {{LOCATION}}{{FLOOR}}
-|mem=Yes
+|mem = Yes
 {{LOCATIONS}}
-|mapID={{MAPID}}{{PLANE}}
+|mapID = {{MAPID}}{{PLANE}}
 }}`;
 
 getIDsAndCoords(ids)
     .then(output => {
-
-        fs.writeFile('output/locations/' + sceneryName.replaceAll(' ','_') + ids.join('_') + '.md', output, err => {
+        if (output.length === 0) {
+            return
+        }
+        fs.writeFile('output/locations/' + sceneryName.replaceAll(' ', '_') + ids.join('_') + '.md', output, err => {
             if (err) {
                 console.error(err);
             }
@@ -45,37 +60,67 @@ function formatData(data) {
     return resultsTable;
 }
 
+async function getRemoteData(id) {
+    const url = `https://mejrs.github.io/data_rs3/locations/${id}.json`
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        if (response.status === 429) {
+            throw new Error(`Failed to fetch data for ID ${id}: Rate limit exceeded`);
+        }
+        throw new Error(`Failed to fetch data for ID ${id}: ${response.statusText}`);
+    }
+    return await response.json();
+}
+
+async function getLocalData(id) {
+    const path = `../DATA_RS3/locations/${id}.json`
+    return JSON.parse(fs.readFileSync(path, 'utf8'));
+}
+
+async function getData(id) {
+    let localData = null;
+    try {
+        localData = await getLocalData(id);
+    }catch(e) {
+        localData = await getRemoteData(id)
+    }
+    return localData.filter(checkUnwantedLocations);
+}
+
+function notFoundParse(item) {
+    return {
+        x: (item.i * 64 + item.x),
+        y: (item.j * 64 + item.y),
+        plane: item.plane,
+        id: item.id
+    };
+}
+
 async function getIDsAndCoords(ids) {
     let results = [];
     let data = [];
     for (let id of ids) {
+
         try {
-            const url = `https://mejrs.github.io/data_rs3/locations/${id}.json`
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                },
-            });
-
-            if (!response.ok) {
-                throw new Error(`Failed to fetch data for ID ${id}: ${response.statusText}`);
-            }
-            let localData = await response.json();
-            localData = localData.filter(checkUnwantedLocations);
-            data = data.concat(localData);
-
+            data = data.concat(await getData(id));
 
         } catch (error) {
             console.error(error);
         }
     }
 
+    data.sort(compareItems)
     let tempData = {};
     for (let item of data) {
         let found = 0;
         let dataY = (item.j * 64 + item.y);
-        let dataX = (item.i * 64 + item.x)
+        let dataX = (item.i * 64 + item.x);
         for (let location of locations) {
             if (dataX > location.minX && dataX < location.maxX && dataY > location.minY && dataY < location.maxY) {
                 const named = location.name + ':::::' + location.mapId
@@ -93,7 +138,8 @@ async function getIDsAndCoords(ids) {
 
         if (found === 0) {
             //TODO: remove once clarification
-            console.log(item);
+            console.log('not found');
+            console.log(notFoundParse(item));
         }
 
     }
@@ -130,8 +176,17 @@ async function getIDsAndCoords(ids) {
 
 function checkUnwantedLocations(item) {
     let dataY = (item.j * 64 + item.y);
-    let dataX = (item.i * 64 + item.x);
+    let dataX = (item.i * 64 + item.x)
+    for (let ignoreLocation of ignoredLocations) {
+        if (dataX > ignoreLocation.minX && dataX < ignoreLocation.maxX && dataY > ignoreLocation.minY && dataY < ignoreLocation.maxY) {
+            return false;
+        }
+    }
 
     return true
     // console.log(object)
+}
+
+function compareItems(a,b){
+    return a.plane-b.plane;
 }
