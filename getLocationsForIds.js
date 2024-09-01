b7e4bcc6c6b1faef3fb550268c14160336ba2fa5
Kasper Sanguesa-Franz <kasper@franz.guru>
2024-09-01 07:55:55 +0100

    Add more locations

--- getLocationsForIds.js
+++ getLocationsForIds.js
@@ -2,12 +2,15 @@ const fs = require('node:fs');
 
 
 // Example usage
-const ids = [	125233];
-const sceneryName = 'Operating table'
+const ids = [121036, 121037];
+const useTemplate2 = true;
+
+
+let sceneryName = 'NOT_FOUND'
 
 
 const locations = [
-    {name: '[[Senntisten]]', west: 1537, east: 2100, north: 1360, south: 1150, mapId: '-1'},
+    {name: '[[Senntisten]]', west: 1535, east: 2112, north: 1417, south: 1088, mapId: '-1'},
     {name: '[[The Zamorakian Undercity]]', west: 1600, east: 1790, north: 1790, south: 1500, mapId: 740},
     {name: '[[Senntisten Asylum]]', west: 1930, east: 2038, north: 1530, south: 1420, mapId: 738},
     {name: '[[Daemonheim]]', west: 3374, east: 3525, north: 3796, south: 3607, mapId: '-1'},
@@ -17,16 +20,34 @@ const locations = [
     {name: '[[The beach]]', west: 3140, east: 3193, north: 3260, south: 3203, mapId: '-1'},
     {name: '[[Al Kharid]]', west: 3266, east: 3334, north: 3221, south: 3133, mapId: '-1'},
     {name: '[[Fort Forinthry]]', west: 3273, east: 3337, north: 3578, south: 3530, mapId: '-1'},
+    {name: '[[Wizards\' Tower]]', west: 3065, east: 3136, north: 3196, south: 3123, mapId: '-1'},
+    {name: '[[Armadyl\'s Tower]]', west: 2980, east: 3000, north: 3276, south: 3254, mapId: '-1'},
+    {name: '[[Void Knights\' Outpost]]', west: 2595, east: 2711, north: 2689, south: 2532, mapId: '-1'},
+    {name: '[[Slayer Tower]]', west: 3396, east: 3451, north: 3579, south: 3537, mapId: '-1'},
+    {name: '[[Gulvas Mansion]]', west: 2937, east: 3010, north: 6275, south: 6211, mapId: '-1'},
+    {name: '[[Robert the Strong\'s library]]', west: 2199, east: 2223, north: 5934, south: 5910, mapId: '-1'},
+    {name: '[[Camdozaal]]', west: 3021, east: 3056, north: 9910, south: 9868, mapId: '-1'},
+    {name: '[[Het\'s Oasis]]', west: 3324, east: 3409, north: 3266, south: 3203, mapId: '-1'},
+    {name: '[[City of Um]]', west: 896, east: 1279, north: 1985, south: 1600, mapId: '742'},
+    {name: '[[Lighthouse]]', west: 2484, east: 2528, north: 3660, south: 3617, mapId: '-1'},
+    {name: '[[]]', west: 0, east: 0, north: 0, south: 0, mapId: '-1'},
+
 
 ];
 
 const ignoredLocations =
     [
-        {name: 'Senntisten instance 1', west: 1595, east: 2049, north: 2054, south: 1791, mapId: '-1'},
-        {name: 'Senntisten instance 2', west: 514, east: 900, north: 529, south: 129, mapId: '-1'},
+        {name: 'Senntisten instance (Twilight of the gods)', west: 1595, east: 2049, north: 2054, south: 1791, mapId: '-1'},
+        {name: 'Senntisten instance (City of Senntisten)', west: 514, east: 900, north: 529, south: 129, mapId: '-1'},
         {name: 'Mort Myre Swamp instance', west: 1984, east: 2050, north: 5056, south: 4990, mapId: '-1'},
         {name: 'Mort Myre Swamp instance', west: 3778, east: 3969, north: 7746, south: 7550, mapId: '-1'},
         {name: 'Fort Forinthry instance', west: 1280, east: 1344, north: 1536, south: 1472, mapId: '-1'},
+        {name: 'Fort Forinthry instance 2', west: 1152, east: 1408, north: 2240 , south: 2111, mapId: '-1'},
+        {name: 'Wizards tower instance', west: 1153, east: 1216, north: 4864, south: 4800, mapId: '-1'},
+        {name: 'South Falador instance', west: 2612, east: 2861, north: 6470, south: 6323, mapId: '-1'},
+        {name: 'South Falador instance', west: 3496, east: 3585, north: 7107, south: 7039, mapId: '-1'},
+        {name: 'South Falador instance', west: 3496, east: 3585, north: 7107, south: 7039, mapId: '-1'},
+        {name: 'lighthouse copy', west: 2415, east: 2496, north: 4634, south: 4544, mapId: '-1'},
     ];
 const template = `{{ObjectLocLine
 |name = {{NAME}}
@@ -36,7 +57,7 @@ const template = `{{ObjectLocLine
 |mapID = {{MAPID}}{{PLANE}}
 }}`;
 
-const template2 = `|map = {{Object map|objectname={{NAME}}|{{LOCATIONS}}}}`
+const template2 = `|map = {{Object map|mapID={{MAPID}}|objectId={{OBJECTID}}{{LOCATIONS}}}}`
 
 getIDsAndCoords(ids)
     .then(output => {
@@ -87,12 +108,22 @@ async function getLocalData(id) {
     return JSON.parse(fs.readFileSync(path, 'utf8'));
 }
 
+async function getLocationConfigLocal(id) {
+    const path = `../DATA_RS3/location_configs/${id}.json`
+    return JSON.parse(fs.readFileSync(path, 'utf8'));
+}
+
 async function getData(id) {
     let localData = null;
     try {
         localData = await getLocalData(id);
     } catch (e) {
         localData = await getRemoteData(id)
+    }
+
+    const nameData1 = await getLocationConfigLocal(id)
+    if(Object.hasOwn(nameData1, 'name')){
+        sceneryName = nameData1['name']
     }
     return localData.filter(checkUnwantedLocations);
 }
@@ -149,7 +180,7 @@ async function getIDsAndCoords(ids) {
     }
 
 
-    if (Object.entries(tempData).length > 0) {
+    if (!useTemplate2 && Object.entries(tempData).length > 0) {
         results.push('{{ObjectTableHead}}')
     }
 
@@ -159,27 +190,31 @@ async function getIDsAndCoords(ids) {
 
         for (const [plane, locationData] of Object.entries(data)) {
 
-            const floorString = multiplePlanes ? " ({{FloorNumber|" + ((+plane) + 1) + "}})" : ''
+            const floorString = plane > 0 ? " ({{FloorNumber|" + ((+plane) + 1) + "}})" : ''
             const planeString = plane > 0 ? "\n|plane=" + plane : ''
-            results.push(
-                template.replaceAll('{{LOCATIONS}}', formatData(locationData).toString().replaceAll(",|", "|"))
-                    .replaceAll('{{LOCATION}}', locationName)
-                    .replaceAll('{{MAPID}}', mapId)
-                    .replaceAll('{{FLOOR}}', floorString)
-                    .replaceAll('{{NAME}}', sceneryName)
-                    .replaceAll('{{PLANE}}', planeString)
-            )
+            if (!useTemplate2) {
+                results.push(
+                    template.replaceAll('{{LOCATIONS}}', formatData(locationData).toString().replaceAll(",|", "|"))
+                        .replaceAll('{{LOCATION}}', locationName)
+                        .replaceAll('{{MAPID}}', mapId)
+                        .replaceAll('{{FLOOR}}', floorString)
+                        .replaceAll('{{NAME}}', sceneryName)
+                        .replaceAll('{{PLANE}}', planeString)
+                )
+                continue;
+            }
             results.push(
                 template2.replaceAll('{{LOCATIONS}}', formatData(locationData).toString().replaceAll(",|", "|"))
                     .replaceAll('{{LOCATION}}', locationName)
                     .replaceAll('{{MAPID}}', mapId)
+                    .replaceAll('{{OBJECTID}}', locationData[0].id)
                     .replaceAll('{{FLOOR}}', floorString)
                     .replaceAll('{{NAME}}', sceneryName)
                     .replaceAll('{{PLANE}}', planeString)
             )
         }
     }
-    if (results.length > 0) {
+    if (!useTemplate2 && results.length > 0) {
         results.push('{{ObjectTableBottom}}');
     }
 
